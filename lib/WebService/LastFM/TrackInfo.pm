1a140a0def97b755f7ed18cf0415aa93c1667a1b
Gene Boggs <gene@ology.net>
2022-06-06 21:50:41 -0700

    Bump version

--- lib/WebService/LastFM/TrackInfo.pm
+++ lib/WebService/LastFM/TrackInfo.pm
@@ -2,7 +2,7 @@ package WebService::LastFM::TrackInfo;
 
 # ABSTRACT: Access to the track.getInfo slice of the LastFM API
 
-our $VERSION = '0.0203';
+our $VERSION = '0.0204';
 
 use Moo;
 use strictures 2;
