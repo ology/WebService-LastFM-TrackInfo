ad83edfa885497937d1e66bf8497065137cef55c
Gene Boggs <gene@ology.net>
2022-06-06 21:56:03 -0700

    Fix ABSTRACT

--- lib/WebService/LastFM/TrackInfo.pm
+++ lib/WebService/LastFM/TrackInfo.pm
@@ -1,6 +1,6 @@
 package WebService::LastFM::TrackInfo;
 
-# ABSTRACT: Access to the track.getInfo slice of the last.fm API
+# ABSTRACT: Access to *.getInfo slices of the last.fm API
 
 our $VERSION = '0.0204';
 
